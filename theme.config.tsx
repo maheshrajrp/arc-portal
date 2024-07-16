afcb8d98eb172abe8e7d7c654b018821ac74cf2b
maheshraj.rp <maheshraj.rp@gmail.com>
2024-07-17 02:26:25 +0530

    Init

---
+++ theme.config.tsx
@@ -0,0 +1,18 @@
+import { DocsThemeConfig } from 'nextra-theme-docs'
+
+const config: DocsThemeConfig = {
+  logo: <span>The Arc</span>,
+  project: {
+    link: 'https://github.com/maheshrajrp',
+  },
+  primaryHue: 191,
+  docsRepositoryBase: 'https://github.com/maheshrajrp',
+  footer: {
+    text: 'The Arc - Iris Club',
+  },
+  search:{ 
+    placeholder: "Search"
+  }
+}
+
+export default config
