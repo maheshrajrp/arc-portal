b6240b18b49f668e083e42b9bc53bc2a007edbaa
maheshraj.rp <maheshraj.rp@gmail.com>
2024-07-18 02:03:42 +0530

    Final commit for the day

--- components/footer.tsx
+++ components/footer.tsx
@@ -3,8 +3,7 @@ import { LinkedIn } from "./icons/linked-in"
 export const Footer = () => {
     return <div className="flex flex-1 w-full justify-between">
         <span>{new Date().getFullYear()} ©{' '}
-            The Arc Project
-            .
+            The Arc Project.
         </span>
         <a href="https://www.linkedin.com/in/maheshraj-r-p-37689b164/" target="_blank">
             <LinkedIn className="h-6 w-6" />
